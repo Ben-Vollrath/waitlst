25795c9cb16bbc3d3bd16c40e37159090fdd7a92
Ben Vollrath <ben.m.vollrath@gmail.com>
2025-01-29 12:34:59 +0100

    Add fullghost button

--- waitlst/src/components/ui/button/index.ts
+++ waitlst/src/components/ui/button/index.ts
@@ -15,6 +15,7 @@ export const buttonVariants = cva(
         secondary:
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
+        fullghost: '',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
