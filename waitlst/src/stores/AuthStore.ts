8ef26c20d14ba933b649362702dd1239df0a0c44
Ben-Vollrath <ben.m.vollrath@gmail.com>
2025-02-02 19:36:02 +0100

    Add auth store

---
+++ waitlst/src/stores/AuthStore.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+import { supabase } from '@/utils/supabase'
+
+interface AuthState {
+  id: string | null
+  email: string | null
+}
+
+export const useAuthStore = defineStore('auth', {
+  state: (): AuthState => ({
+    id: null,
+    email: null,
+  }),
+  actions: {
+
+    async initializeAuth() {
+      // Fetch the current session and set it to the store
+      const { data } = await supabase.auth.getSession()
+      this.id = data?.session?.user?.id || null
+      this.email = data?.session?.user?.email || null
+
+      // Listen to authentication state changes
+      supabase.auth.onAuthStateChange((_, session) => {
+        this.id = session?.user.id || null
+        this.email = session?.user.email || null
+      })
+    },
+  },
+})
