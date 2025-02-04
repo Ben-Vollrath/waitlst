d6613b543dd90af4c51819ee7704e9cd441e312c
Ben Vollrath <ben.m.vollrath@gmail.com>
2025-02-04 22:41:12 +0100

    Add logged in as text

--- waitlst/src/views/DashboardView.vue
+++ waitlst/src/views/DashboardView.vue
@@ -29,7 +29,7 @@ watchEffect(() => {
   <div class="flex items-center justify-center min-h-screen">
     <div class="text-center">
       <h1 class="text-4xl font-bold">Dashboard Coming Soon!</h1>
-      <p class="mt-4 text-lg">We are working on it</p>
+      <p class="mt-4 text-lg">Logged in as {{ authStore.email }}</p>
       <Button @click="handleLogout" class="mt-8">Log out</Button>
     </div>
   </div>
