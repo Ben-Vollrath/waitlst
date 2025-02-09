dca6fb32d1e56aa7b3542b67539cdf630a63dce8
Ben Vollrath <ben.m.vollrath@gmail.com>
2025-02-09 20:11:20 +0100

    Fix cors on waitlist signup endpoint

--- backend/supabase/functions/waitlist-signup/index.ts
+++ backend/supabase/functions/waitlist-signup/index.ts
@@ -1,11 +1,19 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 import { createClient } from "jsr:@supabase/supabase-js@2";
+import { corsHeaders } from '../__shared/cors.ts'
 
 const supabaseUrl = Deno.env.get("SUPABASE_URL") || "";
 const supabaseServiceRoleKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") || "";
 const supabaseClient = createClient(supabaseUrl, supabaseServiceRoleKey);
 
 Deno.serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response(null, {
+      status: 204, // No Content
+      headers: corsHeaders,
+    });
+  }
+
   const { waitlist_id, email, source } = await req.json()
 
   console.log(waitlist_id, email, source)
@@ -20,13 +28,13 @@ Deno.serve(async (req) => {
   if(error) {
     return new Response(
       JSON.stringify({ error: error.message }),
-      { status: 500, headers: { "Content-Type": "application/json" } },
+      { status: 500, headers: { ...corsHeaders, 'Content-Type': 'application/json' } },
     )
   }
 
   return new Response(
     JSON.stringify(waitlistSignup),
-    { headers: { "Content-Type": "application/json" } },
+    { headers: { ...corsHeaders, 'Content-Type': 'application/json' } },
   )
 })
 
