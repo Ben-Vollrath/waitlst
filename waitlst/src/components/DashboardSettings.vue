4f95f90591e4ea5ff0b16a0271520e92438c5fcf
Ben Vollrath <ben.m.vollrath@gmail.com>
2025-02-12 19:29:55 +0100

    Add placeholders

---
+++ waitlst/src/components/DashboardSettings.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+    <div class="flex items-center">
+      <h1 class="text-lg font-semibold md:text-2xl">Settings</h1>
+    </div>
+    <div class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
+      <div class="flex flex-col items-center gap-1 text-center">
+        <h3 class="text-2xl font-bold tracking-tight">Settings here</h3>
+      </div>
+    </div>
+  </main>
+</template>
