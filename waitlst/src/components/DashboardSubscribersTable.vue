fc7a720c3546467309d9f6c39a0035115a6bfb1b
Ben Vollrath <ben.m.vollrath@gmail.com>
2025-02-16 19:09:34 +0100

    Fix subscribers table on mobile

--- waitlst/src/components/DashboardSubscribersTable.vue
+++ waitlst/src/components/DashboardSubscribersTable.vue
@@ -15,25 +15,30 @@ const signupStore = useSignupStore()
 </script>
 
 <template>
-  <Table>
-    <TableHeader>
-      <TableRow>
-        <TableHead>Email</TableHead>
-        <TableHead>Join Date</TableHead>
-        <TableHead> source </TableHead>
-      </TableRow>
-    </TableHeader>
-    <TableBody>
-      <TableRow v-for="signUp in signupStore.signups" :key="signUp.email">
-        <TableCell class="font-medium">
-          {{ signUp.email }}
-        </TableCell>
-        <TableCell>{{ signUp.joined_at }}</TableCell>
-        <TableCell>{{ signUp.source }}</TableCell>
-        <TableCell class="text-right"
-          ><DashboardSubscribersRemoveDialog :email="signUp.email"
-        /></TableCell>
-      </TableRow>
-    </TableBody>
-  </Table>
+  <div class="w-full overflow-hidden">
+    <div class="w-full overflow-x-auto">
+      <Table class="min-w-[600px] w-full">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Email</TableHead>
+            <TableHead>Join Date</TableHead>
+            <TableHead>Source</TableHead>
+            <TableHead class="text-right">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow v-for="signUp in signupStore.signups" :key="signUp.email">
+            <TableCell class="font-medium">
+              {{ signUp.email }}
+            </TableCell>
+            <TableCell>{{ signUp.joined_at }}</TableCell>
+            <TableCell>{{ signUp.source }}</TableCell>
+            <TableCell class="text-right">
+              <DashboardSubscribersRemoveDialog :email="signUp.email" />
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  </div>
 </template>
